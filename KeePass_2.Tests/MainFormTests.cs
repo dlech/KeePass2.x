e43739d0eef8e84eea43a8185adcce7102292741
Justin M <JustinMDotNet@outlook.com>
2025-08-09 18:09:48 -0700

    Added test project to KeePass_2

---
+++ KeePass_2.Tests/MainFormTests.cs
@@ -0,0 +1,16 @@
+﻿using KeePass.Forms;
+
+namespace KeePass.Tests
+{
+    [TestClass]
+    public class MainFormTests
+    {
+        [TestMethod]
+        public void Constructor_DoesNotThrow()
+        {
+            // Basic test to ensure constructor does not throw
+            var form = new MainForm();
+            Assert.IsNotNull(form);
+        }
+    }
+}
