eba10e1669a596dea8cbdfa86b194d932bf3e382
David Lechner <david@lechnology.com>
2025-08-23 15:05:07 -0500

    nuget: fix debug symbols
    Need to have portable debug symbols, otherwise nuget.org rejects the .snupkg.

--- nuget-pack.bat
+++ nuget-pack.bat
@@ -1,1 +1,1 @@
-nuget pack .\KeePass\KeePass_N48.csproj -Build -Symbols -SymbolPackageFormat snupkg
+nuget pack .\KeePass\KeePass_N48.csproj -Build -Symbols -SymbolPackageFormat snupkg -Properties DebugType=portable
